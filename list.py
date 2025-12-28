d03c785f834c25ab1f19cca382031422c6411a7a
Henry-Vo09 <huyhienvo82@gmail.com>
2025-12-27 17:35:39 -0800

    Create list.py
    Added a list of words for the hangman game.

---
+++ list.py
@@ -0,0 +1,7 @@
+#words list for hangman game
+
+words = ( "cat", "dog", "cow", "pig", "hen", "rat", "bat", "ant", "bee", "duck",
+    "tiger", "lion", "zebra", "horse", "panda", "monkey", "rabbit", "sheep", "goat", "fox",
+    "elephant", "giraffe", "kangaroo", "crocodile", "dolphin", "penguin", "octopus", "alligator", "chimpanzee", "porcupine",
+    "hippopotamus", "rhinoceros", "orangutan", "axolotl", "chameleon", "platypus", "armadillo", "anteater", "narwhal", "wolverine",
+    "echidna", "okapi", "quokka", "saiga", "cassowary" )
